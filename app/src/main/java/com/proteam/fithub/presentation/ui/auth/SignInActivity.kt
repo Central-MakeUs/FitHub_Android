de59535137ff0849900e82fc39b9fdaf0f1c9dd8
ChunBaee <mljung10@naver.com>
2023-06-25 21:21:12 +0900

    #5 feat: SignIn with PhoneNumber UI 설정

--- app/src/main/java/com/proteam/fithub/presentation/ui/auth/SignInActivity.kt
+++ app/src/main/java/com/proteam/fithub/presentation/ui/auth/SignInActivity.kt
@@ -1,5 +1,6 @@
 package com.proteam.fithub.presentation.ui.auth
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -14,6 +15,17 @@ class SignInActivity : AppCompatActivity() {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_in)
 
+        initBinding()
 
+
+    }
+
+    private fun initBinding() {
+        binding.activity = this
+    }
+    
+    fun onPhoneSignInClicked() {
+        // TODO: 추후 Result를 받아와, 현재 페이지를 Finish할지 여부를 판단해야 할 듯 하다. 
+        startActivity(Intent(this, SignInWithPhoneNumberActivity::class.java))
     }
 }
---
+++ app/src/main/java/com/proteam/fithub/presentation/ui/auth/SignInWithPhoneNumberActivity.kt
@@ -0,0 +1,41 @@
+package com.proteam.fithub.presentation.ui.auth
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.proteam.fithub.R
+import com.proteam.fithub.databinding.ActivitySigninWithPhoneNumberBinding
+
+class SignInWithPhoneNumberActivity : AppCompatActivity() {
+    private lateinit var binding : ActivitySigninWithPhoneNumberBinding
+    private val TAG = "----"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_signin_with_phone_number)
+        initBinding()
+        initInclude()
+    }
+
+    private fun initBinding() {
+        binding.activity = this
+    }
+
+    private fun initInclude() {
+        binding.signInWithPhoneLayoutPhoneNumber.getAttr("phone")
+        binding.signInWithPhoneLayoutPassword.getAttr("password")
+
+        binding.signInWithPhoneLayoutPhoneNumber.status.observe(this) {phone ->
+            binding.signInWithPhoneLayoutPassword.status.observe(this) {pw ->
+                binding.signInWithPhoneBtnSignIn.isEnabled = phone && pw
+            }
+        }
+    }
+
+    fun onSignInClicked() {
+        // TODO: ID PW 받아온 후 로그인 API 연결하기 
+        Log.d(TAG, "onSignInClicked: ${binding.signInWithPhoneLayoutPhoneNumber.returnInput()} / ${binding.signInWithPhoneLayoutPassword.returnInput()}")
+    }
+}
