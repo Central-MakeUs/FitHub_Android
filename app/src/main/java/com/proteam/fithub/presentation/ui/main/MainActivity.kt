0f2a2dfb38fe5be0ef9f5fff021b268839435708
ChunBaee <mljung10@naver.com>
2023-07-15 21:53:03 +0900

    #22 feat: 메인 액티비티 툴바 및 바텀 네비게이션 구성 완료

--- app/src/main/java/com/proteam/fithub/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/proteam/fithub/presentation/ui/main/MainActivity.kt
@@ -2,11 +2,42 @@ package com.proteam.fithub.presentation.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.proteam.fithub.R
+import com.proteam.fithub.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        initBinding()
+        initUi()
+
     }
+
+    private fun initBinding() {
+        binding.activity = this
+    }
+
+    private fun initUi() {
+        initBottomNavigation()
+    }
+
+    private fun initBottomNavigation() {
+        binding.mainLayoutBottomNavigation.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.main_bottom_home -> changeFragments()
+            }
+            return@setOnItemSelectedListener false
+        }
+    }
+
+    private fun changeFragments(fragment : Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.main_layout_container, fragment).commit()
+    }
+
+
 }
